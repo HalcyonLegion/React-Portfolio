76831cd64aa457ef44f5162cf579967c9e89f891
Lee Webb <leewerbb55@hotmail.com>
2023-05-24 03:47:39 +0800

    using public url for the projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ function Project(props) {
   return (
     <div className="col-md-4">
       <div className="card mb-4">
-        <img className="card-img-top" src={imgSrc} alt="Screenshot or GIF of the application" />
+        <img className="card-img-top" src={`${process.env.PUBLIC_URL}/${imgSrc}`} alt="Screenshot or GIF of the application" />
         <div className="card-body">
           <h3 className="card-title">{title}</h3>
           <a className="btn btn-primary btn-sm" href={deployedUrl}>Deployed Version</a>
