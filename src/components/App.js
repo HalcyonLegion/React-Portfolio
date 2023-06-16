4988f43329b08511a6c9085bc0f3430db0504bd2
Lee Webb <leewerbb55@hotmail.com>
2023-06-16 18:12:38 +0800

    Reverting package.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../App.css";
-import Header from "../components/Header.js";
 import HomePage from "../pages/HomePage.js";
+import Header from "../components/Header.js";
 import ProjectsPage from "../pages/ProjectsPage.js";
 import ContactPage from "../pages/ContactPage.js";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Header />
             <Routes>
                 <Route path="/home" element={<HomePage />} />
