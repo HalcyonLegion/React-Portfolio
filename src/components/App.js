7127b4ae5df307c327b9c585da1973db90c64bee
Lee Webb <leewerbb55@hotmail.com>
2023-06-16 18:18:40 +0800

    implemented useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../App.css";
 import HomePage from "../pages/HomePage.js";
@@ -7,16 +7,19 @@ import ProjectsPage from "../pages/ProjectsPage.js";
 import ContactPage from "../pages/ContactPage.js";
 
 function App() {
-    return (
-        <Router basename={process.env.PUBLIC_URL}>
-            <Header />
-            <Routes>
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/projects" element={<ProjectsPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-        </Router>
-    );
+  const [initialRoute] = useState("/home");
+
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
