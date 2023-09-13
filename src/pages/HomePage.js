4081f43667598e7698ad0749916b233f993e649a
Lee Webb <leewerbb55@hotmail.com>
2023-09-13 21:34:08 +0800

    Updated Portfolio

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,26 +2,28 @@ import React from "react";
 
 function HomePage() {
   return (
-    <section className="container">
+    <section className="container my-5">
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-6 mb-5">
           <h1 className="display-4">Lee Webb</h1>
           <p className="lead">Full-Stack Web Developer</p>
           <p>Welcome to my Portfolio</p>
-          <h2>About Me</h2>
+          <h2 className="mt-4 mb-2">About Me</h2>
           <p>
-            I am an experienced Web Developer with a background in creating and maintaining dynamic
-            websites and web applications. I am skilled in various programming languages and frameworks,
-            including HTML, CSS, JavaScript, Python, Flask, React and Node. A problem-solver with a
-            passion for creating visually appealing, user-friendly and accessible websites. I possess
-            strong attention to detail, and I am deeply fascinated with the potential for new and emerging
-            technologies, particularly AI. I am looking to bring my professional skills to a larger tech
-            company with global reach.
+          I am an experienced Web Developer with a background in creating and maintaining dynamic
+          websites and web applications. Whilst my primary background is in Customer Service, Data
+          and Business Analytics I am looking to pivot into a career I am truly passionate about. I am
+          skilled in various programming languages and frameworks, including HTML, CSS, JavaScript,
+          Python, Flask, React and Node.js. I enjoy tackling problems and have a passion for creating
+          accessible websites that are visually appealing and user-friendly. I possess strong attention to
+          detail, and I am deeply fascinated with the potential for new and emerging technologies,
+          particularly AI. I am looking to bring my professional skills to a larger tech company with global
+          reach.
           </p>
-          <h2>Skills</h2>
+          <h2 className="mt-4 mb-2">Skills</h2>
           <ul>
             <li>HTML</li>
-            <li>CSS / Bootstrap</li>
+            <li>CSS (Bootstrap & Tailwind)</li>
             <li>JavaScript</li>
             <li>React</li>
             <li>Node.js</li>
